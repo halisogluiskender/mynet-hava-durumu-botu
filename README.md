10b672a74d3c87867372a00a8598d9728fddaaf3
halisogluiskender <halisogluiskender@gmail.com>
2019-09-01 20:53:26 +0300

    Mynet hava durumu botu eklemesi

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mynet-hava-durumu-botu
-Maynet hava durumu botu
+# Mynet Hava Durumu Botu
+Basit bir hava durumu botudur. Mynet sütesi üzerinden çekilmektedir. Ama küçük bir problemden bahsetmek istiyorum. Mynet sitesinde iller bazından hava durumu vermekte ama durum ilçelere geldiği zaman birçok ilçe bilgisi olmadığı için bota ilçeleri dahil etmedim.
---
+++ weather.php
@@ -0,0 +1,47 @@
+<?php
+
+function havadurumu($url,$il,$kacgun = 1){
+
+    $il = mb_strtolower($il);
+
+    $veri = file_get_contents($url.'/'.$il);
+    preg_match_all('#<span class="hvDeg1">(.*?)</span>#',$veri,$enYuksek);
+    preg_match_all('#<span class="hvDeg2">(.*?)</span>#',$veri,$enDusuk);
+    preg_match_all('#<span class="hvMood">(.*?)</span>#',$veri,$hvMood);
+    preg_match_all('#<span class="hvDay">(.*?)</span>#',$veri,$hvGun); 
+    
+    if($kacgun!=1 AND $kacgun > 1){
+        $enY=array();
+        $enD=array();
+        $hvM=array();
+        $hvG=array();
+    
+        for($i=0;$i<$kacgun;$i++){
+            array_push($enY,$enYuksek[1][$i]);
+            array_push($enD,$enDusuk[1][$i]);
+            array_push($hvM,$hvMood[1][$i]);
+            array_push($hvG,$hvGun[1][$i]);
+        }
+        $data =array(
+            "enYuksek" => $enY,
+            "enDusuk" => $enD,
+            "durum" => $hvM,
+            "gun" => $hvG
+        );
+        
+    }else{
+        $data = array(
+            "enYuksek" => $enYuksek[1][0],
+            "enDusuk" => $enDusuk[1][0],
+            "durum" => $hvMood[1][0],
+            "gun" => $hvGun[1][0]
+        );
+    }
+
+    return $data;
+
+}
+
+$havaDurumu = havadurumu("http://www.mynet.com/havadurumu/asya/turkiye","Bursa",3);
+
+echo json_encode($havaDurumu);
